55b87d7279e05bfc1bef77df025397b39ae05e8c
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-08-23 03:49:39 +0500

    Add files via upload

---
+++ next-js-css-module/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import CssModule from "@/component/CssModule";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <CssModule/>
+      
+    </main>
+  );
+}
---
+++ next-js-css-module/src/component/CssModule.tsx
@@ -0,0 +1,10 @@
+import style from "../css/CssModule.module.css"
+export default function CssModule(){
+    return(
+        <>
+        <div>
+            <h1 className={style["my-text-color"]}>Learn CSS Module</h1>
+        </div>
+        </>
+    )
+}
