f7be8e6290f00fb8c9e6a30f07de692b81bf90e7
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-08-22 22:14:58 +0500

    Add files via upload

---
+++ layout/src/app/admin/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function AdminDashBoard(){
+    return(
+        <>
+        <div>
+            <h1>Admin DashBoard</h1>
+        </div>
+        </>
+    )
+}
---
+++ layout/src/component/AdminHeader.tsx
@@ -0,0 +1,9 @@
+export default function AdminHeader(){
+    return(
+        <>
+        <div>
+            <h1>Admin Header</h1>
+        </div>
+        </>
+    )
+}
